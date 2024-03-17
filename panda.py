e1ffbcfda732e0e560830d88446e8923d3c77aee
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-17 15:32:50 +0600

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,26 +1,28 @@
-import pandas as pd
-df = pd.read_csv('GooglePlayStore_wild.csv')
-print(df.info())
-print(len(df[pd.isnull(df['Rating'])]))
-df['Rating'].fillna(-1, inplace = True)
-
-print(df['Size'].value_counts())
-def set_size(size):
-   if size[-1] == 'M':
-      return float(size[:-1]) 
-   elif size[-1] == 'k':
-      return float(size[:-1]) / 1024
-   return -1
-df['Size'] = df['Size'].apply(set_size)
-print(df[df['Category'] == 'TOOLS']['Size'].max())
-
-def set_installs(installs):
-   if installs == '0':
-       return 0
-   return int(installs[:-1].replace(',', ''))
-df['Installs'] = df['Installs'].apply(set_installs)
-
-print(round(df.pivot_table(index = 'Content Rating', columns = 'Type', values = 'Installs', aggfunc = 'mean')), 1)
-print(df[pd.isnull(df['Type'])])
-df['Type'].fillna('Free', inplace = True)
-print(df.info())
+import pandas as pd 
+# Шаг 1. Загрузка данных
+df = pd.read_csv('titanic.csv')
+# Шаг 2. Удаление ненужных столбцов
+df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1, inplace = True)
+# Шаг 3. Работа со столбцом портов
+df['Embarked'].fillna('S', inplace = True)
+df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
+df.drop('Embarked', axis = 1, inplace = True)
+# Шаг 4. Работа со столбцом возраста
+age_1 = df[df['Pclass'] == 1]['Age'].median()
+age_2 = df[df['Pclass'] == 2]['Age'].median()
+age_3 = df[df['Pclass'] == 3]['Age'].median()
+def fill_age(row):
+   if pd.isnull(row['Age']):
+       if row['Pclass'] == 1:
+           return age_1
+       if row['Pclass'] == 2:
+           return age_2
+       return age_3
+   return row['Age']
+df['Age'] = df.apply(fill_age, axis = 1)
+# Шаг 5. Работа со столбцом пола
+def fill_sex(sex):
+    if sex == 'male':
+        return 1
+    return 0
+df['Sex'] = df['Sex'].apply(fill_sex)
