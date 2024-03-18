893d881044623fa8952456f6dc6a706a3b77e2d6
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-18 17:17:46 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -1,30 +1,9 @@
-from random import randint
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QWidget,
-                     QLabel, QPushButton, QVBoxLayout)
-'''ИНТЕРФЕЙС'''
+from PyQt5.QtWidgets import (
+    QApplication,QWidget, QPushButton, QLabel,
+    QMessageBox,QRadioButton, QVBoxLayout, QHBoxLayout)
+
 app = QApplication([])
-window = QWidget()
-window.setWindowTitle('Определить победителя')
-window.resize(400,200)
+win = QWidget()
 
-text = QLabel('Кто красавчик?')
-winner = QLabel('?')
-button = QPushButton('Сгенерировать')
-
-layout = QVBoxLayout()
-'''РАЗМЕЩЕНИЕ'''
-
-layout.addWidget(text, alignment=Qt.AlignCenter)
-layout.addWidget(winner, alignment=Qt.AlignCenter)
-layout.addWidget(button)
-
-window.setLayout(layout)
-
-def show_winner():
-    winner.setText(['Кутман','Роман','Арсений','Артем'][randint(0,3)])
-
-button.clicked.connect(show_winner)
-
-window.show()
+win.show()
 app.exec()
