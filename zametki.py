f48aa807a487714b4e3000392175587a254094ef
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-18 15:19:31 +0600

    Update zametki.py

--- zametki.py
+++ zametki.py
@@ -107,17 +107,34 @@ def del_note():
         list_notes.clear()
         list_tags.clear()
         field_text.clear()
-        
+
         with open('notes_data.json','w') as file:
             json.dump(notes, file)
 
         list_notes.addItems(notes)
 
-
-
 '''Работа с тегами заметки'''
 
+def add_tag():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        tag = field_tag.text()
+        if not tag in notes[key]['теги']:
+            notes[key]['теги'].append(tag)
+            list_tags.addItem(tag)
+            field_tag.clear()
+        with open('notes_data.json','w') as file:
+            json.dump(notes, file)
 
+def del_tag():
+    if list_tags.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        tag = list_tags.selectedItems()[0].text()
+        notes[key]['теги'].remove(tag)
+        list_tags.clear()
+        list_tags.addItems(notes[key]['теги'])
+        with open('notes_data.json','w') as file:
+            json.dump(notes, file)
 
 '''Запуск приложения'''
 #подключение обработки событий
@@ -125,8 +142,8 @@ button_note_create.clicked.connect(add_note)
 list_notes.itemClicked.connect(show_note)
 button_note_save.clicked.connect(save_note)
 button_note_del.clicked.connect(del_note)
-#button_tag_add.clicked.connect(add_tag)
-#button_tag_del.clicked.connect(del_tag)
+button_tag_add.clicked.connect(add_tag)
+button_tag_del.clicked.connect(del_tag)
 #button_tag_search.clicked.connect(search_tag)
 
 
