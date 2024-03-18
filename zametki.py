99c5f2cb020ae6e8df713a1db7aefde4530220ea
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-18 14:57:18 +0600

    Update zametki.py

--- zametki.py
+++ zametki.py
@@ -100,6 +100,20 @@ def save_note():
         with open("notes_data.json", "w") as file:
             json.dump(notes, file)
 
+def del_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        del notes[key]
+        list_notes.clear()
+        list_tags.clear()
+        field_text.clear()
+        
+        with open('notes_data.json','w') as file:
+            json.dump(notes, file)
+
+        list_notes.addItems(notes)
+
+
 
 '''Работа с тегами заметки'''
 
@@ -110,7 +124,7 @@ def save_note():
 button_note_create.clicked.connect(add_note)
 list_notes.itemClicked.connect(show_note)
 button_note_save.clicked.connect(save_note)
-#button_note_del.clicked.connect(del_note)
+button_note_del.clicked.connect(del_note)
 #button_tag_add.clicked.connect(add_tag)
 #button_tag_del.clicked.connect(del_tag)
 #button_tag_search.clicked.connect(search_tag)
