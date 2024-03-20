2f8a820ae754749b3b9d41c7e7c98841509fe570
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-20 16:55:47 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -1,50 +1,14 @@
-from PyQt5.QtWidgets import (
-    QApplication,QWidget, QLabel,
-    QMessageBox,QRadioButton, QVBoxLayout, QHBoxLayout)
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QVBoxLayout, QHBoxLayout,
+    QGroupBox, QLabel,
+    QRadioButton, QPushButton)
 
+#Интерфейс
 app = QApplication([])
 win = QWidget()
-win.resize(200,200)
-win.setWindowTitle('Тест на дебилизм')
 
-text = QLabel('Сколько будет 2 + 2')
-
-answer1 = QRadioButton('5')
-answer2 = QRadioButton('пять')
-answer3 = QRadioButton('низнаю')
-answer4 = QRadioButton('4')
-
-main = QVBoxLayout()
-h1 = QHBoxLayout()
-h2 = QHBoxLayout()
-
-main.addWidget(text, alignment = Qt.AlignCenter)
-
-h1.addWidget(answer1, alignment = Qt.AlignLeft)
-h1.addWidget(answer2, alignment = Qt.AlignLeft)
-h2.addWidget(answer3, alignment = Qt.AlignLeft)
-h2.addWidget(answer4, alignment = Qt.AlignLeft)
-
-main.addLayout(h1)
-main.addLayout(h2)
-
-win.setLayout(main)
-
-def you_lose():
-    result = QMessageBox()
-    result.setText('Поздравляем вы дебил')
-    result.exec()
-
-def you_win():
-    result = QMessageBox()
-    result.setText('Поздравляем вы не дебил')
-    result.exec()
-
-answer1.clicked.connect(you_lose)
-answer2.clicked.connect(you_lose)
-answer3.clicked.connect(you_lose)
-answer4.clicked.connect(you_win)
-
+# Запуск
 win.show()
 app.exec()
