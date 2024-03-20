8fb9428d11117fc094ed9bfb2573c74e6a2c2219
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-20 15:29:53 +0600

    Update image.py

--- image.py
+++ image.py
@@ -11,3 +11,29 @@ with Image.open('файл') as original:
     perevorot.save('norm.jpg')
     #вывод изображения
     perevorot.show()
+
+
+
+
+#подключи нужные модули PIL
+from PIL import Image, ImageFilter
+#создай класс ImageEditor
+class ImageEditor():
+    #создай конструктор класса
+    def __init__(self, filename):
+        self.filename = filename
+        self.original = None
+        self.changed = []
+    #создай метод "открыть и показать оригинал"
+    def open(self):
+        try:
+            self.original = Image.open(self.filename).show()
+        except:
+            print('Файл не найден')
+
+    #создай методы для редактирования оригинала
+#создай объект класса ImageEditor с данными картинки-оригинала
+image1 = ImageEditor('lion.jpg')
+image1.open()
+
+#отредактируй изображение и сохрани результат
