605dc29c3be46f27946edf2e143d932dfdc6fa05
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-20 17:20:51 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -13,9 +13,27 @@ win.setWindowTitle('Викторина')
 # Интерфейс
 question = QLabel('Ты кто по жизни?')
 button = QPushButton('Ответить')
+# Интерфейс группы с ответами
+answers = QGroupBox('Варианты')
+rbnt1 = QRadioButton('Чел')
+rbnt2 = QRadioButton('Пес')
+rbnt3 = QRadioButton('Гитлер')
+rbnt4 = QRadioButton('Анимешник')
+#Размещение внутри группы с ответами
+h = QHBoxLayout()
+v1 = QVBoxLayout()
+v2 = QVBoxLayout()
+v1.addWidget(rbnt1)
+v1.addWidget(rbnt2)
+v2.addWidget(rbnt3)
+v2.addWidget(rbnt4)
+h.addLayout(v1)
+h.addLayout(v2)
+answers.setLayout(h)
 # Размещение
 main_layout = QVBoxLayout()
 main_layout.addWidget(question, alignment = Qt.AlignHCenter)
+main_layout.addWidget(answers)
 main_layout.addWidget(button)
 win.setLayout(main_layout)
 # Запуск
