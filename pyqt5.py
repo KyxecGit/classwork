a16db8b72da05a95055fb7e02e24fd981aeec1d0
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-03-25 16:29:32 +0600

    Update pyqt5.py

--- pyqt5.py
+++ pyqt5.py
@@ -1,51 +1,92 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QApplication, QWidget,
-    QVBoxLayout, QHBoxLayout,
-    QGroupBox, QLabel,
-    QRadioButton, QPushButton)
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
 
 app = QApplication([])
 
-win = QWidget()
-win.setWindowTitle('Викторина')
 
-# Интерфейс
-question = QLabel('Ты кто по жизни?')
-button = QPushButton('Ответить')
-# Интерфейс группы с ответами
-answers = QGroupBox('Варианты')
-rbnt1 = QRadioButton('Чел')
-rbnt2 = QRadioButton('Пес')
-rbnt3 = QRadioButton('Гитлер')
-rbnt4 = QRadioButton('Анимешник')
-# Интерфейс окна с результатом
-results = QGroupBox('Результат')
-result = QLabel('Правильно')
-right_answer = QLabel('Гитлер')
-# Размещение внутри группы с ответами
-h = QHBoxLayout()
-v1 = QVBoxLayout()
-v2 = QVBoxLayout()
-v1.addWidget(rbnt1)
-v1.addWidget(rbnt2)
-v2.addWidget(rbnt3)
-v2.addWidget(rbnt4)
-h.addLayout(v1)
-h.addLayout(v2)
-answers.setLayout(h)
-# Размещение внутри группы с результатом
-v = QVBoxLayout()
-v.addWidget(result)
-v.addWidget(right_answer, alignment = Qt.AlignHCenter)
-results.setLayout(v)
-# Размещение
-main_layout = QVBoxLayout()
-main_layout.addWidget(question, alignment = Qt.AlignHCenter)
-main_layout.addWidget(answers)
-main_layout.addWidget(results)
-main_layout.addWidget(button)
-win.setLayout(main_layout)
-# Запуск
-win.show()
+btn_OK = QPushButton('Ответить') # кнопка ответа
+lb_Question = QLabel('Самый сложный вопрос в мире!') # текст вопроса
+
+
+RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
+
+
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+
+RadioGroup = QButtonGroup() # это для группировки переключателей, чтобы управлять их поведением
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+
+
+RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+
+
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+layout_line1 = QHBoxLayout() # вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() # кнопка "Ответить"
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() # скроем панель с ответом, сначала должна быть видна панель вопросов
+
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+
+
+layout_card = QVBoxLayout()
+
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memo Card')
+
+
+window.resize(400, 300)
+window.show()
 app.exec()
